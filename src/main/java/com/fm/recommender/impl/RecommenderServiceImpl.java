8dfad415937638e760cf94431005361979d782cc
Almir <crafteasy@yandex.ru>
2024-09-01 17:18:39 +0300

    Добавлен класс пара(movie, score) изменен метод getTop()

--- src/main/java/com/fm/recommender/impl/RecommenderServiceImpl.java
+++ src/main/java/com/fm/recommender/impl/RecommenderServiceImpl.java
@@ -8,33 +8,45 @@ import java.util.stream.Collectors;
 
 
 public class RecommenderServiceImpl implements RecommenderService<Movie, User> {
-
     final Scorer<Movie, User> scorer = new ScorerImpl();
     private List<Movie> listMovies = new ArrayList<>();
     @Override
     public List<Movie> getTop(User user, int limit) {
         int currentListSize = listMovies.size();
-             List<Movie> compileMoviesForUser = listMovies.stream().sorted(new Comparator<Movie>() {
-                @Override
-                public int compare(Movie o1, Movie o2) {
-                    double scoreMovie1 = scorer.getScore(o1, user);
-                    double scoreMovie2 = scorer.getScore(o2, user);
-                    if (scoreMovie1 == scoreMovie2) {
-                        return 0;
-                    } else if (scoreMovie1 > scoreMovie2) {
-                        return -1;
-                    } else return 1;
-                }
-            }).collect(Collectors.toList());
-             if (currentListSize < limit) {
-                 return compileMoviesForUser;
-             }
-            return compileMoviesForUser.subList(0, limit);
-    }
+        List<Movie> compileMoviesForUser = listMovies.stream().map(movie -> new CreatePair(movie,scorer.getScore(movie, user))).sorted(new Comparator<CreatePair>() {
+            @Override
+            public int compare(CreatePair o1, CreatePair o2) {
+                if (o1.getScore() == o2.getScore()) {
+                    return 0;
+                } else if (o1.getScore() > o2.getScore()) {
+                    return -1;
+                } else return 1;
+            }
+        }).map(m -> m.getMovie()).collect(Collectors.toList());
 
+        if (currentListSize < limit) {
+            return compileMoviesForUser;
+        }
+        return compileMoviesForUser.subList(0, limit);
+    }
     @Override
     public void addMovie(Movie movie) {
         listMovies.add(movie);
+    }
+
+    static class CreatePair {
+        private Movie movie;
+        private double score;
+        CreatePair(Movie movie, double score) {
+            this.movie = movie;
+            this.score = score;
+        }
+        public double getScore() {
+            return score;
+        }
+        public Movie getMovie() {
+            return movie;
+        }
     }
 }
 
