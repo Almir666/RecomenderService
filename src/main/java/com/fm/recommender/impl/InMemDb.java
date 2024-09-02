f6bf7749ced056efb8844bf4b91886a551ad9b98
Almir <crafteasy@yandex.ru>
2024-09-02 16:07:54 +0300

    Продолжае работу, добавляем базу данных для хранения фильмов

---
+++ src/main/java/com/fm/recommender/impl/InMemDb.java
@@ -0,0 +1,17 @@
+package com.fm.recommender.impl;
+
+import com.fm.recommender.Db;
+
+import java.util.List;
+
+public class InMemDb implements Db {
+    @Override
+    public List<Movie> getAllMovies() {
+        return null;
+    }
+
+    @Override
+    public void saveMovie(Movie movie) {
+
+    }
+}
