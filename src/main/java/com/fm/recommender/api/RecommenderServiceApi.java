dfb5a2220b62063a0b23bb06f7757ea6bb33d922
Almir <crafteasy@yandex.ru>
2024-09-08 18:46:16 +0300

    Подправил зависимости

--- src/main/java/com/fm/recommender/api/RecommenderServiceApi.java
+++ src/main/java/com/fm/recommender/api/RecommenderServiceApi.java
@@ -3,6 +3,7 @@ package com.fm.recommender.api;
 import com.fm.recommender.core.impl.Movie;
 import com.fm.recommender.core.impl.RecommenderServiceImpl;
 import com.fm.recommender.core.impl.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,11 +11,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class RecommenderServiceApi {
-
     private RecommenderServiceImpl recommenderService;
 
-    @GetMapping("/recommender/getTop")
-    public List<Movie> getTop(User user, int limit) {
+    @GetMapping("/recommender/getTop/{limit}")
+    public List<Movie> getTop(@PathVariable int limit, @RequestBody User user) {
         return recommenderService.getTop(user, limit);
     }
 
