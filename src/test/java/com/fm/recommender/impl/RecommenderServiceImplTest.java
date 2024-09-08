3623cf22d5cf29aa792711d8d8d77f41d3b496cf
Almir <crafteasy@yandex.ru>
2024-09-08 15:06:28 +0300

    учусь делать revert

--- src/test/java/com/fm/recommender/impl/RecommenderServiceImplTest.java
+++ src/test/java/com/fm/recommender/impl/RecommenderServiceImplTest.java
@@ -12,6 +12,7 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 public class RecommenderServiceImplTest {
+
     @Test
     void testGetTopOverLimit() {
         Movie movie1 = new Movie(
@@ -42,12 +43,12 @@ public class RecommenderServiceImplTest {
         db.saveMovie(movie3);
         db.saveMovie(movie2);
         db.saveMovie(movie1);
+
+        final RecommenderService<Movie, User> recomender = new RecommenderServiceImpl(scorer, db);
         List<Movie> actual = new ArrayList<>();
         actual.add(movie2);
         actual.add(movie3);
         actual.add(movie1);
-
-        final RecommenderService<Movie, User> recomender = new RecommenderServiceImpl(scorer, db);
         List<Movie> expected = recomender.getTop(user, 5);
         assertIterableEquals(expected, actual);
     }
@@ -81,62 +82,12 @@ public class RecommenderServiceImplTest {
         db.saveMovie(movie3);
         db.saveMovie(movie2);
         db.saveMovie(movie1);
-        List<Movie> actual = new ArrayList<>();
-        actual.add(movie2);
-        actual.add(movie3);
 
         final RecommenderService<Movie, User> recomender = new RecommenderServiceImpl(scorer, db);
-        List<Movie> expected = recomender.getTop(user, 2);
-        assertIterableEquals(expected, actual);
-    }
-    @Test
-    void testRebootApplication() {
-        Movie movie1 = new Movie(
-                UUID.randomUUID().toString(),
-                "Twilight",
-                "When Bella Swan moves to a small town in the Pacific Northwest, she falls in love with Edward Cullen, a mysterious classmate who reveals himself to be a 108-year-old vampire.",
-                new double[]{1., 2.}
-        );
-        Movie movie2 = new Movie(
-                UUID.randomUUID().toString(),
-                "Lord of the rings",
-                "In the Second Age of Middle-earth, the lords of Elves, Dwarves, and Men are given Rings of Power. Unbeknownst to them, the Dark Lord Sauron forges the One Ring in Mount Doom, instilling into it a great part of his power to dominate the other Rings and conquer Middle-earth.",
-                new double[]{5., 6.}
-        );
-        Movie movie3 = new Movie(
-                UUID.randomUUID().toString(),
-                "Chainsaw Man",
-                "Chainsaw Man follows the story of Denji, an impoverished teenager who makes a contract that fuses his body with that of a dog-like Devil named Pochita, granting him the ability to transform parts of his body into chainsaws.",
-                new double[]{3., 2.}
-        );
-        Movie movie4 = new Movie(
-                UUID.randomUUID().toString(),
-                "Game of Thrones",
-                "Set on the fictional continents of Westeros and Essos, Game of Thrones has a large ensemble cast and follows several story arcs throughout the course of the show. The first major arc concerns the Iron Throne of the Seven Kingdoms of Westeros through a web of political conflicts among the noble families either vying to claim the throne or fighting for independence from whoever sits on it.",
-                new double[]{4., 5.}
-        );
-        User user = new User(
-                UUID.randomUUID().toString(),
-                "Almir",
-                new double[]{1., 2.}
-        );
-
-        final Db db = new InMemDb();
-        final Scorer<Movie, User> scorer = new ScorerImpl();
         List<Movie> actual = new ArrayList<>();
         actual.add(movie2);
-        actual.add(movie4);
         actual.add(movie3);
-        actual.add(movie1);
-
-        final RecommenderService<Movie, User> recomender1 = new RecommenderServiceImpl(scorer, db);
-        final RecommenderService<Movie, User> recomender2 = new RecommenderServiceImpl(scorer, db);
-        recomender1.addMovie(movie1);
-        recomender1.addMovie(movie2);
-        recomender1.addMovie(movie3);
-        recomender1.addMovie(movie4);
-
-        List<Movie> expected = recomender2.getTop(user, 4);
+        List<Movie> expected = recomender.getTop(user, 2);
         assertIterableEquals(expected, actual);
     }
 }
