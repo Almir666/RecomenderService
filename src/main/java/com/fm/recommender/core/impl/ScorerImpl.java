195a1b409361b2b577b34b2d4ea1c06c8cda7679
Almir <crafteasy@yandex.ru>
2024-09-13 23:42:39 +0300

    Добавили лог

--- src/main/java/com/fm/recommender/core/impl/ScorerImpl.java
+++ src/main/java/com/fm/recommender/core/impl/ScorerImpl.java
@@ -1,19 +1,24 @@
 package com.fm.recommender.core.impl;
 
 import com.fm.recommender.core.Scorer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class ScorerImpl implements Scorer<Movie, User> {
+
+    private final static Logger log = LoggerFactory.getLogger(ScorerImpl.class);
     @Override
     public double getScore(Movie movie, User user) {
         double[] movieEmbedding = movie.getEmbedding();
         double[] userEmbedding  = user.getEmbedding();
         if (movieEmbedding.length != userEmbedding.length) {
-            throw new RuntimeException("user embedding and movie embedding have different sizes");
+            log.warn("user embedding and movie embedding have different sizes");
         }
         double score = 0;
-        for (int i = 0; i < movieEmbedding.length; i++) {
+        for (int i = 0; i < Math.min(movieEmbedding.length, userEmbedding.length); i++) {
             score += movieEmbedding[i] * userEmbedding[i];
         }
         return score;
